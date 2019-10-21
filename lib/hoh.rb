0492a4ac654ee4b7eca7f8d78c2bff8b4aed3343
Timothy Rines <tsrines@gmail.com>
2019-10-21 14:58:02 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,6 +31,28 @@
 # }
 
 def naming_system
+  {label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+              }
+            }
+        
+          }
+        }
+      }
+    }
+}
+
   # Remember:
   #  Kingdom
   #  Phylum
